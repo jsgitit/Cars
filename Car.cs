09806ebbca099b31e0d08deb8aed1099e845a4d2
Jon Smart <jsmartware@gmail.com>
2020-07-21 16:43:08 -0700

    Added Car.cs

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Displacement { get; set; }
+        public int Cylinders { get; set; }
+        public double City { get; set; }
+        public double Highway { get; set; }
+        public double Combined { get; set; }
+    }
+}
