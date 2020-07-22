fc78ed30c0b884fb40460104f73772db9604be2f
Jon Smart <jsmartware@gmail.com>
2020-07-22 09:29:54 -0700

    Added Manufacturer.cs for Joins.  Has Composite Key examples as well.

--- Program.cs
+++ Program.cs
@@ -9,11 +9,13 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            var cars = ProcessFile("fuel.csv");
+            var cars = ProcessCars("fuel.csv");
+            var manufacturers = ProcessManufacturers("manufacturers.csv");
+
             var query = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
                             .OrderByDescending(c => c.Combined)
                             .ThenBy(c => c.Name)
-                            .Select(c => new { c.Manufacturer, c.Name, c.Combined}); // select just these 3 properties
+                            .Select(c => new { c.Manufacturer, c.Name, c.Combined }); // select just these 3 properties
 
             // Alternative LINQ Query Syntax
             // var query = 
@@ -51,9 +53,98 @@ namespace Cars
             {
                 Console.Write(character);
             }
+
+            // Joins with Query Syntax
+            var query_FuelEfficientManufacturers_QuerySyntax =
+                from car in cars
+                join manufacturer in manufacturers on car.Manufacturer equals manufacturer.Name
+                orderby car.Combined descending, car.Name ascending
+                select new
+                {
+                    manufacturer.Headquarters,
+                    car.Name,
+                    car.Combined
+                };
+            foreach (var car in query_FuelEfficientManufacturers_QuerySyntax.Take(10))
+            {
+                Console.WriteLine($"HQ: {car.Headquarters} - CarName: {car.Name}  - MPG: {car.Combined}");
+            }
+
+            Console.WriteLine("***");
+            // Joins with Method Syntax
+            var query_FuelEfficientManufacturers_MethodSyntax =
+                cars.Join(manufacturers, c => c.Manufacturer,
+                                        m => m.Name, (c, m) => new  // project fields we need
+                                        {
+                                            m.Headquarters,
+                                            c.Name,
+                                            c.Combined
+                                        })
+                    .OrderByDescending(c => c.Combined)
+                    .ThenBy(c => c.Name);
+            foreach (var car in query_FuelEfficientManufacturers_MethodSyntax.Take(10))
+            {
+                Console.WriteLine($"HQ: {car.Headquarters} - CarName: {car.Name}  - MPG: {car.Combined}");
+            }
+
+            // Joins with a composite key requires we build an object.
+            // Query Syntax approach
+
+            Console.WriteLine("Joining with a composite key (query syntax)");
+            var query_FuelEfficientManufacturers_CompositeKey_QuerySyntax =
+                from car in cars
+                join manufacturer in manufacturers
+                    on new { car.Manufacturer, car.Year }
+                        equals
+                        new { Manufacturer = manufacturer.Name, manufacturer.Year }
+                orderby car.Combined descending, car.Name ascending
+                select new
+                {
+                    manufacturer.Headquarters,
+                    car.Name,
+                    car.Combined
+                };
+            foreach (var car in query_FuelEfficientManufacturers_CompositeKey_QuerySyntax.Take(10))
+            {
+                Console.WriteLine($"HQ: {car.Headquarters} - CarName: {car.Name}  - MPG: {car.Combined}");
+            }
+
+            Console.WriteLine("Joins with composite key (Method Syntax)");
+            // Joins with Method Syntax
+            var query_FuelEfficientManufacturers_CompositeKey_MethodSyntax =
+                cars.Join(manufacturers, c => new { c.Manufacturer, c.Year },
+                                        m => new { Manufacturer = m.Name, m.Year }, (c, m) => new  // project fields we need
+                                        {
+                                            m.Headquarters,
+                                            c.Name,
+                                            c.Combined
+                                        })
+                    .OrderByDescending(c => c.Combined)
+                    .ThenBy(c => c.Name);
+            foreach (var car in query_FuelEfficientManufacturers_CompositeKey_MethodSyntax.Take(10))
+            {
+                Console.WriteLine($"HQ: {car.Headquarters} - CarName: {car.Name}  - MPG: {car.Combined}");
+            }
         }
 
-        private static List<Car> ProcessFile(string path)
+        private static List<Manufacturer> ProcessManufacturers(string path)
+        {
+            var query = File.ReadAllLines(path)
+                            .Where(line => line.Length > 1)
+                            .Select(line =>
+                            {
+                                var columns = line.Split(',');
+                                return new Manufacturer
+                                {
+                                    Name = columns[0],
+                                    Headquarters = columns[1],
+                                    Year = int.Parse(columns[2])
+                                };
+                            });
+            return query.ToList();
+        }
+
+        private static List<Car> ProcessCars(string path)
         {
             var query = File.ReadAllLines(path)
                         .Skip(1) // skip header
