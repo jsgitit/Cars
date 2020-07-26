dfc212a12201902844d0fc6b47c7a57df1b46c7a
Jon Smart <jsmartware@gmail.com>
2020-07-26 16:31:02 -0700

    Refactored prior to adding LINQ XML and EF code

--- Program.cs
+++ Program.cs
@@ -1,95 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Threading;
 
 namespace Cars
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var cars = ProcessCars("fuel.csv");
             var manufacturers = ProcessManufacturers("manufacturers.csv");
 
-            var query = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
-                            .OrderByDescending(c => c.Combined)
-                            .ThenBy(c => c.Name)
-                            .Select(c => new { c.Manufacturer, c.Name, c.Combined }); // select just these 3 properties
+            GetTopBMWsForYear(cars, 2016);
+            DemonstrateQuantifiers(cars);
+            DemonstrateProjections(cars);
+            DemonstrateJoins(cars, manufacturers);
+            DemonstrateCompositeJoins(cars, manufacturers);
+            FindTwoMostFuelEfficientCarsByManufacturer(cars);
+            GroupJoinQuery(cars, manufacturers);
+            GroupByCountryShowTopThreeCars(cars, manufacturers);
+            GetManufacturerStatistics(cars);
+        }
 
-            // Alternative LINQ Query Syntax
-            // var query = 
-            //      from car in cars
-            //      orderby car.Combined descending, car.Name ascending
-            //      select car;
-
-            foreach (var car in query.Take(10))
-            {
-                Console.WriteLine($"Car Name: {car.Name} - MPG {car.Combined}");
-            }
-            var topCar = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
-                            .OrderByDescending(c => c.Combined)
-                            .ThenBy(c => c.Name)
-                            .Select(c => c)
-                            .FirstOrDefault();  // executes query, makes query concrete. might return null
-
-            Console.WriteLine($"Top car: {topCar.Name}");
-
-            // Quantifiers
-            var result = cars.Any(c => c.Manufacturer == "Ford"); // Any Fords? (True)
-            Console.WriteLine(result);
-
-            result = cars.All(c => c.Manufacturer == "Ford"); // Are all Cars Fords? (False)
-            Console.WriteLine(result);
-
-            // Projections - "Select" transforms to anything
-            // .Select(c => new { c.Manufacturer, c.Name, c.Combined}); // select just these 3 properties
-
-            // SelectMany() flattens a sequence of sequences 
-            // for example, each char name is a sequence of characters.
-            var carName = cars.SelectMany(c => c.Name)
-                              .OrderBy(c => c);
-            foreach (var character in carName)
-            {
-                Console.Write(character);
-            }
-
-            // Joins with Query Syntax
-            var query_FuelEfficientManufacturers_QuerySyntax =
-                from car in cars
-                join manufacturer in manufacturers on car.Manufacturer equals manufacturer.Name
-                orderby car.Combined descending, car.Name ascending
-                select new
-                {
-                    manufacturer.Headquarters,
-                    car.Name,
-                    car.Combined
-                };
-            foreach (var car in query_FuelEfficientManufacturers_QuerySyntax.Take(10))
-            {
-                Console.WriteLine($"HQ: {car.Headquarters} - CarName: {car.Name}  - MPG: {car.Combined}");
-            }
-
-            Console.WriteLine("***");
-            // Joins with Method Syntax
-            var query_FuelEfficientManufacturers_MethodSyntax =
-                cars.Join(manufacturers, c => c.Manufacturer,
-                                        m => m.Name, (c, m) => new  // project fields we need
-                                        {
-                                            m.Headquarters,
-                                            c.Name,
-                                            c.Combined
-                                        })
-                    .OrderByDescending(c => c.Combined)
-                    .ThenBy(c => c.Name);
-            foreach (var car in query_FuelEfficientManufacturers_MethodSyntax.Take(10))
-            {
-                Console.WriteLine($"HQ: {car.Headquarters} - CarName: {car.Name}  - MPG: {car.Combined}");
-            }
+        private static void DemonstrateCompositeJoins(List<Car> cars, List<Manufacturer> manufacturers)
+        {
 
             // Joins with a composite key requires we build an object.
             // Query Syntax approach
@@ -130,25 +65,109 @@ namespace Cars
                 Console.WriteLine($"HQ: {car.Headquarters} - CarName: {car.Name}  - MPG: {car.Combined}");
             }
 
-            Console.WriteLine("***");
-            FindTwoMostFuelEfficientCarsByManufacturer("Fuel.csv");
-
-            Console.WriteLine("***"); 
-            GroupJoinQuery("Fuel.csv");
-
-            Console.WriteLine("***");
-            GroupByCountryShowTopThreeCars("Fuel.csv", "Manufacturers.csv");
-
-            Console.WriteLine("***");
-            GetManufacturerStatistics("Fuel.csv", "Manufacturers.csv");
         }
 
-        private static void FindTwoMostFuelEfficientCarsByManufacturer(string path)
+        private static void DemonstrateJoins(List<Car> cars, List<Manufacturer> manufacturers)
         {
-            // Find the 2 most fuel efficient cars for each Manufacturer
+            Console.WriteLine("*** Joining cars and manufacturers with LINQ ***");
+
+            // Joins with Query Syntax
+            var query_FuelEfficientManufacturers_QuerySyntax =
+                from car in cars
+                join manufacturer in manufacturers on car.Manufacturer equals manufacturer.Name
+                orderby car.Combined descending, car.Name ascending
+                select new
+                {
+                    manufacturer.Headquarters,
+                    car.Name,
+                    car.Combined
+                };
+            foreach (var car in query_FuelEfficientManufacturers_QuerySyntax.Take(10))
+            {
+                Console.WriteLine($"HQ: {car.Headquarters} - CarName: {car.Name}  - MPG: {car.Combined}");
+            }
+
+            Console.WriteLine("***");
+            // Joins with Method Syntax
+            var query_FuelEfficientManufacturers_MethodSyntax =
+                cars.Join(manufacturers, c => c.Manufacturer,
+                                        m => m.Name, (c, m) => new  // project fields we need
+                                        {
+                                            m.Headquarters,
+                                            c.Name,
+                                            c.Combined
+                                        })
+                    .OrderByDescending(c => c.Combined)
+                    .ThenBy(c => c.Name);
+            foreach (var car in query_FuelEfficientManufacturers_MethodSyntax.Take(10))
+            {
+                Console.WriteLine($"HQ: {car.Headquarters} - CarName: {car.Name}  - MPG: {car.Combined}");
+            }
+        }
+
+        private static void DemonstrateProjections(List<Car> cars)
+        {
+            Console.WriteLine("*** Demonstrating Projections and SelectMany() ***");
+
+            // Projections - "Select" transforms to anything
+            // .Select(c => new { c.Manufacturer, c.Name, c.Combined}); // select just these 3 properties
+
+            // SelectMany() flattens a sequence of sequences 
+            // for example, each char name is a sequence of characters.
+            var carName = cars.SelectMany(c => c.Name)
+                              .OrderBy(c => c);
+            foreach (var character in carName)
+            {
+                Console.Write(character);
+            }
+
+        }
+
+        private static void DemonstrateQuantifiers(List<Car> cars)
+        {
+            Console.WriteLine("*** Demonstrating Quantifiers***");
+
+            // Quantifiers
+
+            var result = cars.Any(c => c.Manufacturer == "Ford"); // Any Fords? (True)
+            Console.WriteLine("Any Fords? " + result);
+
+            result = cars.All(c => c.Manufacturer == "Ford"); // Are all Cars Fords? (False)
+            Console.WriteLine("Are all cars fords? " + result);
+        }
+
+        private static void GetTopBMWsForYear(List<Car> cars, int year)
+        {
+            Console.WriteLine("*** Get Top BMWs for 2016 ***");
+            var query = cars.Where(c => c.Manufacturer == "BMW" && c.Year == year)
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name)
+                            .Select(c => new { c.Manufacturer, c.Name, c.Combined }); // select just these 3 properties
+
+            // Alternative LINQ Query Syntax
+            // var query = 
+            //      from car in cars
+            //      orderby car.Combined descending, car.Name ascending
+            //      select car;
+
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"Car Name: {car.Name} - MPG {car.Combined}");
+            }
+            var topCar = cars.Where(c => c.Manufacturer == "BMW" && c.Year == year)
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name)
+                            .Select(c => c)
+                            .FirstOrDefault();  // executes query, makes query concrete. might return null
+
+            Console.WriteLine($"Top car: {topCar.Name}");
+        }
+
+        private static void FindTwoMostFuelEfficientCarsByManufacturer(List<Car> cars)
+        {
+            Console.WriteLine("*** Find the 2 most fuel efficient cars for each Manufacturer ***");
 
             // Use LINQ Query Syntax
-            var cars = ProcessCars("fuel.csv");
             var query_GroupByManufacturerQuerySyntax =
                 from car in cars
                 group car by car.Manufacturer.ToUpper() into manufacturer
@@ -177,15 +196,12 @@ namespace Cars
                     Console.WriteLine($"\t{car.Name} has a combined fuel efficiency of {car.Combined} MPG");
                 }
             }
-
-
         }
 
-        private static void GroupJoinQuery(string path)
+        private static void GroupJoinQuery(List<Car> cars, List<Manufacturer> manufacturers)
         {
-            var cars = ProcessCars("fuel.csv");
-            var manufacturers = ProcessManufacturers("manufacturers.csv");
-            
+            Console.WriteLine("*** Using GroupJoin() LINQ Extension Method ***");
+
             // Use LINQ Query Syntax
             var queryGroupJoinQuerySyntax =
                 from manufacturer in manufacturers
@@ -209,10 +225,10 @@ namespace Cars
             // Use LINQ Method Syntax
             var queryGroupJoinMethodSyntax =
                 manufacturers.GroupJoin(cars, m => m.Name, c => c.Manufacturer, (m, g) => new
-                    {
-                        Manufacturer = m,
-                        Cars = g
-                    })
+                {
+                    Manufacturer = m,
+                    Cars = g
+                })
                 .OrderBy(m => m.Manufacturer.Name);
             foreach (var group in queryGroupJoinMethodSyntax)
             {
@@ -224,11 +240,9 @@ namespace Cars
             }
         }
 
-        private static void GroupByCountryShowTopThreeCars(string FuelPath, string ManufacturerPath)
+        private static void GroupByCountryShowTopThreeCars(List<Car> cars, List<Manufacturer> manufacturers)
         {
-            // Get some data
-            var cars = ProcessCars(FuelPath);
-            var manufacturers = ProcessManufacturers(ManufacturerPath);
+            Console.WriteLine("*** Group By Headquarters Country, then show Top 3 Cars ***");
 
             var queryGroupJoinQuerySyntax =
                 from manufacturer in manufacturers
@@ -275,15 +289,13 @@ namespace Cars
             }
         }
 
-        private static void GetManufacturerStatistics(string FuelPath, string ManufacturerPath)
+        private static void GetManufacturerStatistics(List<Car> cars)
         {
-            // Get some data
-            var cars = ProcessCars(FuelPath);
-            var manufacturers = ProcessManufacturers(ManufacturerPath);
+            Console.WriteLine("*** Get Manufacturer Statistics *** ");
 
             var queryGetStatsQuerySyntax =
                 from car in cars
-                group car by car.Manufacturer 
+                group car by car.Manufacturer
                 into carGroup
                 select new
                 {
@@ -300,7 +312,7 @@ namespace Cars
                 Console.Write($"{result.Name}\t\t\t\t\t");
                 Console.Write($"{result.Min}\t");
                 Console.Write($"{result.Max}\t");
-                Console.WriteLine("F02: {0}",result.Avg);
+                Console.WriteLine("F02: {0}", result.Avg);
             }
 
             Console.WriteLine("***");
@@ -333,6 +345,7 @@ namespace Cars
 
         private static List<Manufacturer> ProcessManufacturers(string path)
         {
+            Console.WriteLine("*** Reading all Manufacturers from file ***");
             var query = File.ReadAllLines(path)
                             .Where(line => line.Length > 1)
                             .Select(line =>
@@ -348,9 +361,9 @@ namespace Cars
             return query.ToList();
         }
 
-        
         private static List<Car> ProcessCars(string path)
         {
+            Console.WriteLine("*** Reading all Cars from file ***");
             var query = File.ReadAllLines(path)
                         .Skip(1) // skip header
                         .Where(line => line.Length > 1) // skip last line
@@ -363,9 +376,7 @@ namespace Cars
             //            where line.Length > 1
             //            select Car.ParseFromCSV(line);
             //return query.ToList();
-            
+
         }
-
-
     }
 }
